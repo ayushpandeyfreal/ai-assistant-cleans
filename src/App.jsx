68486b84c6905d4c26d68abe5517d304ec9fa93d
Ayush Pandey <ayushji2555@gmail.com>
2026-03-25 21:00:24 +0530

    updated backend url

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   setLoading(true);
 
   try {
-    const res = await axios.post("http://localhost:5000/chat", {
+    const res = await axios.post("https://ai-assistant-backends.onrender.com/chat", {
       messages: updatedMessages,
     });
 
